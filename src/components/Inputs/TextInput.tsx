e940b0109cfefe5ad46c47fca8d86d28737677a5
IgnaSZa <ignas.zakar@gmail.com>
2025-02-18 03:43:24 +0200

    Improved header

--- src/components/Inputs/TextInput.tsx
+++ src/components/Inputs/TextInput.tsx
@@ -1,32 +1,34 @@
 import { MutableRefObject, useEffect, useState } from "react";
 
 export type TextInputPropsType = {
-  name: string
-  value?: string,
+  name: string;
+  value?: string;
   dataRef?: MutableRefObject<{ [key: string]: any }>;
-}
+};
 
 export function TextInput(props: TextInputPropsType) {
-  const {name, value: propValue, dataRef } = props;
-  const [value, setValue] = useState(propValue || '');
+  const { name, value: propValue, dataRef } = props;
+  const [value, setValue] = useState(propValue || "");
 
   useEffect(() => {
     if (!dataRef) return;
     dataRef.current = {
-        ...dataRef.current,
-        [name]: value
-    }
+      ...dataRef.current,
+      [name]: value,
+    };
   }, [name, value]);
 
   useEffect(() => {
-    setValue(propValue || '');
+    setValue(propValue || "");
   }, [propValue]);
 
-  return <input
-    className='flex flex-row border rounded-md bg-gray-100 p-1 focus-within:outline-2 focus-within:outline-gray-600'
-    type="text"
-    value={value}
-    placeholder={name}
-    onChange={(e) => setValue(e.target.value)}
-  />
+  return (
+    <input
+      className="flex col-span-2 border rounded-md bg-gray-100 p-1 focus-within:outline-2 focus-within:outline-gray-600"
+      type="text"
+      value={value}
+      placeholder={name}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 }
--- src/components/modals/TaskCreationModal.tsx
+++ src/components/modals/TaskCreationModal.tsx
@@ -1,10 +1,10 @@
-import { MutableRefObject, useRef } from 'react';
-import { BaseModalWrapper } from './BaseModalWrapper';
-import { useDispatch } from 'react-redux';
-import { toggleTaskListRefetchState } from '../../redux/slices/vehicleListRefetchSlice';
-import VehicleSelect from '../selects/VehicleSelect';
-import { TextInput } from '../Inputs/TextInput';
-import { DateInput } from '../Inputs/DateInput';
+import { MutableRefObject, useRef } from "react";
+import { BaseModalWrapper } from "./BaseModalWrapper";
+import { useDispatch } from "react-redux";
+import { toggleTaskListRefetchState } from "../../redux/slices/vehicleListRefetchSlice";
+import VehicleSelect from "../selects/VehicleSelect";
+import { TextInput } from "../Inputs/TextInput";
+import { DateInput } from "../Inputs/DateInput";
 
 type PropsType = {
   openRef: MutableRefObject<() => void>;
@@ -17,30 +17,26 @@ export function TaskCreationModal(props: PropsType) {
   const dataRef = useRef<{ [key: string]: any }>({});
 
   const dispatch = useDispatch();
-  
+
   function submitTask() {
     console.log(dataRef?.current);
     window.create.createTask(dataRef?.current);
-    dispatch(toggleTaskListRefetchState())
+    dispatch(toggleTaskListRefetchState());
     closeRef.current();
   }
 
   return (
-    <BaseModalWrapper
-      closeRef={closeRef}
-      openRef={openRef}>
-        <div className='grid grid-cols-2 gap-2'>
-           <VehicleSelect dataRef={dataRef} />
-           <TextInput name='note' dataRef={dataRef} />
-           <DateInput name='task_date' dataRef={dataRef} />
-        <div className='flex justify-center col-span-2'>
-          <button
-            className='rounded-sm'
-            onClick={submitTask}>
+    <BaseModalWrapper closeRef={closeRef} openRef={openRef}>
+      <div className="grid grid-cols-2 gap-2">
+        <VehicleSelect dataRef={dataRef} />
+        <DateInput name="task_date" dataRef={dataRef} />
+        <TextInput name="note" dataRef={dataRef} />
+        <div className="flex justify-center col-span-2">
+          <button className="rounded-sm" onClick={submitTask}>
             Save
           </button>
         </div>
-        </div>
+      </div>
     </BaseModalWrapper>
   );
 }
